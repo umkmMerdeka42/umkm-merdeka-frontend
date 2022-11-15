9dcdb69586cd4e8006eef6bc49eb42998d5e42d8
AjiPerdana38 <ajicooljazz38@gmail.com>
2022-11-15 20:22:12 +0700

    edit footer

--- src/scripts/components/footer.js
+++ src/scripts/components/footer.js
@@ -21,13 +21,13 @@ class FooterBar extends HTMLElement {
             <a href="#" aria-label="github" class="hover:text-gray-600 transition-all text-3xl">
             <i class="fa-brands fa-square-github"></i>
             </a>
-            <a href=" https://wa.me/6285695951121" aria-label="send wa" class="hover:text-gray-600 transition-all text-3xl">
+            <a href=" https://wa.me/6285695951121" target="_blank" rel="noopener noreferrer" aria-label="send wa" class="hover:text-gray-600 transition-all text-3xl">
             <i class="fa-brands fa-square-whatsapp"></i>
             </a>
-            <a href="#" title="instagram" target="blank" aria-label="facebook" class="hover:text-gray-600 transition-all text-3xl">
+            <a href="#" title="instagram" aria-label="facebook" class="hover:text-gray-600 transition-all text-3xl">
               <i class="fa-brands fa-square-instagram"></i>
             </a>
-            <a href="#" title="linkedin" target="blank" aria-label="linkedin" class="hover:text-gray-600 transition-all text-3xl">
+            <a href="#" title="email" aria-label="linkedin" class="hover:text-gray-600 transition-all text-3xl">
               <i class="fa-brands fa-linkedin"></i>
             </a>
           </div>
