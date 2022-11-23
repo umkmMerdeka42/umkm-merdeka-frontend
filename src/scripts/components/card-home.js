aa6f1938acc31901bc092f74ee8abb1ebdf08b71
AjiPerdana38 <ajicooljazz38@gmail.com>
2022-11-24 00:48:37 +0700

    fix bug dark

--- src/scripts/components/card-home.js
+++ src/scripts/components/card-home.js
@@ -17,10 +17,10 @@ class CardHome extends HTMLElement {
             alt="art cover" loading="lazy" width="1000" height="667" class="h-64 w-full object-cover object-top transition duration-500 group-hover:scale-105"/>
           </div>
           <div class="mt-6 relative">
-            <h3 class="text-2xl font-semibold text-gray-800 dark:text-white">
+            <h3 class="text-2xl font-semibold text-gray-800">
               ${data.productName}
             </h3>
-            <p class="mt-6 mb-8 text-gray-600 dark:text-gray-300">
+            <p class="mt-6 mb-8 text-gray-600">
               ${data.description.slice(0, 100)}...
             </p>
             <a class="inline-block" href="#">
