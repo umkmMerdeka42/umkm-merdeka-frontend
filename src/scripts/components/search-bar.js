8eecef3415ca3da596bef473bc9aa69592e5c1a2
ikhsan rangkuti <mhd.al.ikhsan24@gmail.com>
2022-12-04 23:45:59 +0700

    add workbox

--- src/scripts/components/search-bar.js
+++ src/scripts/components/search-bar.js
@@ -7,7 +7,7 @@ class SearchBar extends HTMLElement {
     this.innerHTML = /* html */ `
         <div class="form-control">
             <div class="input-group rounded-md border w-32 md:w-60 py-3 px-4">
-            <input id="searchElement" type="search" placeholder="Search…" class="input input-bordered outline-none text-sm" />
+            <input id="searchElement" type="text" placeholder="Search…" class="input input-bordered outline-none text-sm" />
             <button id="searchButtonElement" class="btn btn-square align-middle">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
             </button>
