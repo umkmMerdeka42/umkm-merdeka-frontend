06c56a18e952bd638b5aa810068c3299bbd58687
AjiPerdana38 <ajicooljazz38@gmail.com>
2022-12-11 20:58:53 +0700

    setting url footer

--- src/scripts/components/footer.js
+++ src/scripts/components/footer.js
@@ -19,16 +19,16 @@ class FooterBar extends HTMLElement {
               <li role="listitem"><a href="#/about-us" class="hover:text-gray-600 transition-all">Tentang Kami</a></li>
             </ul>
             <div class="m-auto flex w-max items-center justify-between space-x-4">
-              <a href="#" aria-label="github" class="hover:text-gray-600 transition-all text-3xl">
+              <a href="https://github.com/umkmMerdeka42" aria-label="github" class="hover:text-gray-600 transition-all text-3xl">
               <i class="fa-brands fa-square-github"></i>
               </a>
               <a href=" https://wa.me/6285695951121" target="_blank" rel="noopener noreferrer" aria-label="send wa" class="hover:text-gray-600 transition-all text-3xl">
               <i class="fa-brands fa-square-whatsapp"></i>
               </a>
-              <a href="#" title="instagram" aria-label="facebook" class="hover:text-gray-600 transition-all text-3xl">
+              <a href="https://www.instagram.com/nafasyarahma_" title="instagram" aria-label="facebook" class="hover:text-gray-600 transition-all text-3xl">
                 <i class="fa-brands fa-square-instagram"></i>
               </a>
-              <a href="#" title="email" aria-label="linkedin" class="hover:text-gray-600 transition-all text-3xl">
+              <a href="https://www.linkedin.com/in/m-aji-perdana-3807071bb/" title="email" aria-label="linkedin" class="hover:text-gray-600 transition-all text-3xl">
                 <i class="fa-brands fa-linkedin"></i>
               </a>
             </div>
